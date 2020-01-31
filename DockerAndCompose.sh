2b61a62170177872f1fde816c5d6c25477b4a1d0
nvko <nvko@frsupc088>
2020-01-31 14:40:24 +0100

    DockerAndCompose.sh

--- DockerAndCompose.sh
+++ DockerAndCompose.sh
@@ -26,11 +26,11 @@ LOGFILE=$WRK_DIR/docker-and-compose.log
 
 # Functions definition
 dependencies() {
+  apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
   curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
   apt-key fingerprint 0EBFCD88
   add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
   apt update
-  apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
 }
 
 docker_ins() {
